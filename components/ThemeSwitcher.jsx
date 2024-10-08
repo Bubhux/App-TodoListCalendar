fd31fec87d664e178c0080cfdc27c562a046ad15
Bubhux <bubhuxpaindepice@gmail.com>
2024-10-08 12:34:40 +0200

    Mise à jour V.4.4  Changes to be committed: 	modified:   components/ThemeSwitcher.jsx 	modified:   hooks/useTheme.js 	deleted:    sass/components/_calendar-interactions.scss 	modified:   sass/components/_theme-switcher.scss 	modified:   sass/layout/_app-container.scss 	modified:   sass/main.scss 	modified:   src/index.css 	modified:   src/index.css.map

--- components/ThemeSwitcher.jsx
+++ components/ThemeSwitcher.jsx
@@ -1,18 +1,35 @@
 // components/ThemeSwitcher.jsx
+import React from "react";
+import useTheme from "../hooks/useTheme.js";
 
 
-const ThemeSwitcher = ({
-    isDark,
-    onChange,
-    invertedIconLogic = false }) => (
+/**
+ * Composant ThemeSwitcher permettant de basculer entre les thèmes clair et sombre.
+ *
+ * @param {Object} props - Les propriétés du composant.
+ * @param {boolean} [props.invertedIconLogic=false] - Si vrai, l'icône est inversée par rapport à l'état du thème.
+ * @returns {JSX.Element} Le composant ThemeSwitcher.
+ */
+const ThemeSwitcher = ({ invertedIconLogic = false }) => {
+    // Récupération du thème actuel et de la fonction pour basculer le thème
+    const { theme, toggleTheme } = useTheme()
+    
+    // Détermine si le thème actuel est sombre
+    const isDark = theme === "dark"
+
+    return (
         <label className={`theme__container ${isDark ? "IsDark" : "IsLight"}`}>
+            {/* Case à cocher pour changer le thème */}
             <input
                 type="checkbox"
+                // La case est cochée en fonction de l'état du thème et de la logique inversée des icônes
                 defaultChecked={invertedIconLogic ? !isDark : isDark}
-                onChange={onChange}
+                // Fonction appelée lorsque l'état de la case change
+                onChange={toggleTheme}
             />
             <div />
         </label>
-)
+    )
+}
 
 export default ThemeSwitcher;
