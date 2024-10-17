ebf1c33405f1491b81adec1233b90d55cc210a94
Bubhux <bubhuxpaindepice@gmail.com>
2024-10-17 16:51:32 +0200

    Mise à jour V.4.8  Changes to be committed: 	modified:   components/ThemeSwitcher.jsx 	modified:   sass/base/_variables.scss 	modified:   sass/components/_calendar-month-body.scss 	modified:   sass/components/_calendar-month-header.scss 	modified:   sass/components/_calendar-time-date.scss 	modified:   sass/components/_todo-list-group.scss 	modified:   sass/layout/_app-container.scss 	modified:   sass/utils/_animations.scss 	modified:   src/App.jsx 	modified:   src/index.css 	modified:   src/index.css.map

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from '../components/TodoList.jsx';
 import Calendar from '../components/Calendar.jsx';
 import ScrollAnimation from '../components/ScrollAnimation.jsx';
@@ -7,11 +7,28 @@ import ThemeSwitcher from '../components/ThemeSwitcher.jsx';
 import { DateHoverProvider } from '../components/DateHoverContext.jsx';
 
 
+/**
+ * Composant principal de l'application.
+ * Gère le changement de thème et force la réinitialisation des animations en modifiant la clé du composant principal.
+ *
+ * @returns {JSX.Element} Le composant App.
+ */
 const App = () => {
+    // État pour gérer la clé unique du composant principal
+    const [themeKey, setThemeKey] = useState(0)
+
+    /**
+     * Fonction appelée lors du changement de thème.
+     * Incrémente la clé du composant principal pour forcer le re-render,
+     * ce qui réinitialise les animations.
+     */
+    const handleThemeChange = () => {
+        setThemeKey(prevKey => prevKey + 1)
+    }
 
     return (
         <DateHoverProvider>
-            <div className="app__container reveal">
+            <div className="app__container reveal" key={themeKey}>
                 <ScrollAnimation />
                 <div className="header">
                     <div className="logo__container">
@@ -19,7 +36,7 @@ const App = () => {
                     </div>
                     <div className="theme__wrapper">
                         <div className="theme__container">
-                            <ThemeSwitcher />
+                            <ThemeSwitcher onChange={handleThemeChange} />
                         </div>
                     </div>
                 </div>
